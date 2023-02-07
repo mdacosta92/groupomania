13346dd6dbb54aabfdec77007655ac8982620d0c
mdacosta92 <79317416+mdacosta92@users.noreply.github.com>
2023-02-07 19:53:38 +0100

    Add files via upload

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -1,51 +1,60 @@
 const Post = require('../model/post');
 
 exports.likePost = (req, res, next) => {
-    console.log("-->CONTENU req.body.like - ctrl like")
-    console.log(req.body.like)
+	Post.findOne({_id: req.params.id})
+		.then((postObject) => {
+			if (
+				!(postObject.usersLiked.includes(req.user._id) || postObject.usersDisliked.includes(req.user._id)) &&
+				req.body.like === 1
+			) {
 
-    console.log("-->CONTENU req.body.userId - ctrl user")
-    console.log(req.body.userId)
+				Post.updateOne({_id: req.params.id},
+					{
+						$inc: {likes: 1},
+						$push: {usersLiked: req.user._id}
+					}
+				)
+					.then(res.status(201).json({message: "post likée"}))
+					.catch((error) => res.status(500).json({error}));
 
-    console.log('--> id en _id')
-    console.log({ _id: req.params.id })
+			} else if (
+				!(postObject.usersDisliked.includes(req.user._id) || postObject.usersLiked.includes(req.user._id)) &&
+				req.body.like === -1
+			) {
 
-    Post.findOne({ _id: req.params.id })
-        .then((postObject) => {
+				Post.updateOne({_id: req.params.id},
+					{
+						$inc: {dislikes: 1},
+						$push: {usersDisliked: req.user._id}
+					}
+				)
+					.then(res.status(201).json({message: "post dislikée"}))
+					.catch((error) => res.status(500).json({error}));
 
-            if (!postObject.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+			} else if (postObject.usersLiked.includes(req.user._id) && req.body.like === 0) {
 
-                Post.updateOne({ _id: req.params.id },
-                    {
-                        $inc: { likes: 1 },
-                        $push: { usersLiked: req.body.userId }
-                    }
-                ).then()(res.status(201).json({ message: "post likée" })
-                ).catch((error) => res.status(500).json({ error }));
+				Post.updateOne({_id: req.params.id},
+					{
+						$inc: {likes: -1},
+						$pull: {usersLiked: req.user._id}
+					}
+				)
+					.then(res.status(201).json({message: "like enlevé"}))
+					.catch((error) => res.status(500).json({error}));
 
-            } else if (postObject.usersLiked.includes(req.body.userId) && req.body.like === 0) {
+			} else if (postObject.usersDisliked.includes(req.user._id) && req.body.like === 0) {
+				Post.updateOne({_id: req.params.id},
+					{
+						$inc: {dislikes: -1},
+						$pull: {usersDisliked: req.user._id}
+					}
+				)
+					.then(res.status(201).json({message: "dislike enlevé"}))
+					.catch((error) => res.status(500).json({error}));
 
-                Post.updateOne({ _id: req.params.id },
-                    {
-                        $inc: { likes: -1 },
-                        $pull: { usersLiked: req.body.userId }
-                    }
-                ).then()(res.status(201).json({ message: "like enlevé" })
-                ).catch((error) => res.status(500).json({ error }));
-                
-            } else if (!postObject.usersDisliked.includes(req.body.userId) && req.body.like === -1) {
+			} else {
+				res.status(400).json({message: 'bad request'});
+			}
 
-                Post.updateOne({ _id: req.params.id },
-                    {
-                        $inc: { dislikes: 1 },
-                        $push: { usersDisliked: req.body.userId }
-                    }
-                ).then()(res.status(201).json({ message: "post dislikée" })
-                ).catch((error) => res.status(500).json({ error }));
-
-            } else {
-                res.status(400).json({ message: 'bad request' })
-            }
-
-        }).catch((error) => res.status(404).json({ error }))
-}
+		}).catch((error) => res.status(404).json({error}));
+};
