cb8d400d9d975b09457e2c8ae2334cc37a64db09
mdacosta92 <79317416+mdacosta92@users.noreply.github.com>
2023-02-07 19:55:18 +0100

    Add files via upload

--- src/manager/PostManager.js
+++ src/manager/PostManager.js
@@ -1,12 +1,26 @@
-import axios from "axios";
-import AbstractManager from "./AbstractManager";
+import axios from 'axios';
+import AbstractManager from './AbstractManager';
 
-export default new class PostManager extends AbstractManager {
-    getAllPosts() {
-        return axios.get(AbstractManager.apiUrl + '/posts', {
-            headers: {
-                Authorization: `Bearer ${AbstractManager.token}`
-            }
-        });
-    }
-};
+export default new (class PostManager extends AbstractManager {
+  getAllPosts() {
+    return axios.get(AbstractManager.apiUrl + '/posts', {
+      headers: {
+        Authorization: `Bearer ${AbstractManager.token}`,
+      },
+    });
+  }
+
+  likeModify(postId, status) {
+    return axios.post(
+      'http://localhost:5000/api/posts/like/' + postId,
+      {
+        like: status,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${AbstractManager.token}`,
+        },
+      }
+    );
+  }
+})();
